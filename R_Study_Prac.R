34e7d02b365e9782d6a8c476572b4170873482fa
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2022-10-24 00:39:02 +0900

    Update R_Study_Prac

--- R_Study_Prac.R
+++ R_Study_Prac.R
@@ -31,4 +31,28 @@ mean(df_example$가격)
 mean(df_example$판매량)
 
 ## 003
-# Q1
+# Q1. ggplot2 패키지의 mpg 데이터를 사용할 수 있도록 불러온 뒤 복사본을 만드세요.
+library(ggplot2)
+mpg <- as.data.frame(ggplot2::mpg)
+mpg
+# Q2. 복사본 데이터를 이용해서 cty는 city로, hwy는 highway로 변수명을 수정하세요.
+mpg <- rename(mpg, city = cty, highway = hwy)
+mpg
+
+# 문제1. ggplot2의 midwest 데이터를 데이터 프레임 형태로 불러와서 데이터의 특성을 파악하세요.
+midwest <- as.data.frame(ggplot2::midwest)
+head(midwest)
+# 문제2. poptotal(전체 인구)을 total로, popasian(아시아 인구)을 asian으로 변수명을 수정하세요.
+midwest <- rename(midwest, total = poptotal, asian = popasian)
+head(midwest)
+# 문제3. total, asian 변수를 이용해 '전체 인구 대비 아시아 인구 백분율' 파생변수를 만들고, 히스토그램을 만들어 도시들이 어떻게 분포하는지 살펴보세요.
+midwest$per_asian <- (midwest$asian / midwest$total * 100)
+head(midwest$per_asian)
+hist(midwest$per_asian)
+# 문제4. 아시아 인구 백분율 전체 평균을 구하고, 평균을 초과하면 "large", 그 외에는 "small"을 부여하는 파생변수를 만들어 보세요.
+midwest$many_asian <-
+  ifelse(midwest$per_asian > mean(midwest$per_asian), "large", "small")
+midwest$many_asian
+# 문제5. "large"와 "small"에 해당하는 지역이 얼마나 되는지, 빈도표와 빈도 막대 그래프를 만들어 확인해 보세요.
+table(midwest$many_asian)
+qplot(midwest$many_asian)
